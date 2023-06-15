f3c11c8236b33f581f4769d8a3c718165345052d
Jaschp <jaschparmar@gmail.com>
2023-06-15 14:34:05 +0200

    application

--- src/main/java/com/example/notlokicoin/Launcher.java
+++ src/main/java/com/example/notlokicoin/Launcher.java
@@ -2,7 +2,8 @@ package com.example.notlokicoin;
 
 public class Launcher {
     public static void main(String[] args){
-        TimePass.run();
+        Simulation sm = new Simulation();
+        sm.run();
         //System.out.println(Simulation.getMinerArray());
     }
 }
