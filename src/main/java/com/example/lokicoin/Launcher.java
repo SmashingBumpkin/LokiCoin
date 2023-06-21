fd72d3c554d0da02469d952ae0809b3a88b4c0fe
SmashingBumpkin <66125276+SmashingBumpkin@users.noreply.github.com>
2023-06-21 09:03:57 +0200

    Simple launcher fix

--- src/main/java/com/example/lokicoin/Launcher.java
+++ src/main/java/com/example/lokicoin/Launcher.java
@@ -3,14 +3,19 @@ package com.example.lokicoin;
 import com.example.lokicoin.blockchain.Miner;
 import com.example.lokicoin.blockchain.Simulation;
 
-public class Launcher {
+public class Launcher{
     public static void main(String[] args){
-        Simulation sm = new Simulation();
-        sm.run();
-//        try{
-//            Thread.sleep(3000);
-//        } catch (Exception e){
-//        }
-//        Miner.minersActive = false;
+        Thread simThread = new Thread(() -> {
+                Simulation sm = new Simulation();
+                sm.run();
+        });
+        simThread.start();
+        try{
+            System.out.println("____________________Starting sim__________________________________________");
+            Thread.sleep(10000);
+            System.out.println("____________________Stopping sim__________________________________________");
+        } catch (Exception e){
+        }
+        Miner.minersActive = false;
     }
 }
